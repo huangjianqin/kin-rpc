9b7b2eff2a1cdf972b084e0196ddae9fbd7bad6d
huangjianqin <18814127639@163.com>
2019-08-22 18:33:15 +0800

    抛弃javassist字节码生成技术, 因为单单从服务方法动态调用来看, 确实性能提高了很多, 但是从整个调用过程来看, 其实提升并不大, 而且加大了设计的复杂度和需要增加处理类卸载的逻辑, 得不偿失, 不如直接反射, 简单粗暴

--- kin-rpc-core/kin-rpc-api/src/test/java/org/kin/kinrpc/provider/Invoker.java
+++
@@ -1,9 +0,0 @@
-package org.kin.kinrpc.provider;
-
-/**
- * @author huangjianqin
- * @date 2019-08-22
- */
-public interface Invoker {
-    void invoke();
-}
--- kin-rpc-core/kin-rpc-api/src/test/java/org/kin/kinrpc/provider/Sayable.java
+++
@@ -1,9 +0,0 @@
-package org.kin.kinrpc.provider;
-
-/**
- * @author huangjianqin
- * @date 2019-08-21
- */
-public interface Sayable {
-    void say();
-}
