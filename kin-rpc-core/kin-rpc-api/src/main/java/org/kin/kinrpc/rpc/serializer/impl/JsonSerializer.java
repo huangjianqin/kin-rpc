06339fd20282a26b1bd679e7d09f88ad1782eafb
huangjianqin <18814127639@163.com>
2019-12-27 21:56:55 +0800

    将fastjson替换会jackson

--- kin-rpc-core/kin-rpc-api/src/main/java/org/kin/kinrpc/rpc/serializer/impl/JsonSerializer.java
+++ kin-rpc-core/kin-rpc-api/src/main/java/org/kin/kinrpc/rpc/serializer/impl/JsonSerializer.java
@@ -1,6 +1,6 @@
 package org.kin.kinrpc.rpc.serializer.impl;
 
-import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.kin.kinrpc.rpc.serializer.Serializer;
 
 import java.io.IOException;
@@ -12,15 +12,15 @@ import java.nio.charset.Charset;
  */
 public class JsonSerializer implements Serializer {
     private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public byte[] serialize(Object target) throws IOException {
-        return JSON.toJSONString(target).getBytes();
+        return mapper.writeValueAsBytes(target);
     }
 
     @Override
-    public <T> T deserialize(byte[] bytes, Class<T> tagetClass) throws IOException, ClassNotFoundException {
-        String json = new String(bytes, UTF8);
-        return JSON.parseObject(json, tagetClass);
+    public <T> T deserialize(byte[] bytes, Class<T> tagetClass) throws IOException {
+        return mapper.readValue(bytes, tagetClass);
     }
 }
