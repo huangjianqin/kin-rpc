afe8caebd974d03d146b422f1e82129282f19c8f
huangjianqin <18814127639@163.com>
2020-06-08 16:06:56 +0800

    逻辑优化

--- kin-rpc-core/kin-rpc-api/src/main/java/org/kin/kinrpc/rpc/RPCThreadPool.java
+++ kin-rpc-core/kin-rpc-api/src/main/java/org/kin/kinrpc/rpc/RPCThreadPool.java
@@ -10,17 +10,17 @@ import org.kin.framework.utils.SysUtils;
  */
 public class RPCThreadPool {
     /**
-     * invoker公用线程池, 除了服务provider,netty和服务注册以外, 都用这个线程池
+     * invoker公用线程池, 除了服务provider,netty以外, 都用这个线程池
      */
     public static final ExecutionContext EXECUTORS =
-            ExecutionContext.forkjoin(SysUtils.getSuitableThreadNum() * 2 - 1, "rpc-common", 5, "rpc-common-schedule");
+            ExecutionContext.forkjoin(SysUtils.getSuitableThreadNum(), "rpc-common", 5, "rpc-common-schedule");
 
     /**
      * 服务请求处理线程池
      * 可以在加载类RPCProvider前修改RPC.parallelism来修改RPCProvider的并发数
      */
     public static ExecutionContext PROVIDER_WORKER =
-            ExecutionContext.fix(KinRPC.PARALLELISM, "rpc-provider", 2, "rpc-provider-schedule");
+            ExecutionContext.fix(SysUtils.getSuitableThreadNum(), "rpc-provider", 2, "rpc-provider-schedule");
 
     //添加JVM关闭钩子,以确保释放该静态线程池
     static {
