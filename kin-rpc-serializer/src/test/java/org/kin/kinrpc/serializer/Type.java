ccf19dd9fa259937e9810c4a55a59c0381aadfc9
huangjianqin <18814127639@163.com>
2020-11-27 17:11:02 +0800

    序列化测试修改

---
+++ kin-rpc-serializer/src/test/java/org/kin/kinrpc/serializer/Type.java
@@ -0,0 +1,55 @@
+package org.kin.kinrpc.serializer;
+
+/**
+ * 序列化测试的类
+ *
+ * @author huangjianqin
+ * @date 2020/11/27
+ */
+public class Type {
+    private int a;
+    private String b;
+    private Object copy;
+
+    public Type() {
+    }
+
+    public Type(int a, String b, Object copy) {
+        this.a = a;
+        this.b = b;
+        this.copy = copy;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public Object getCopy() {
+        return copy;
+    }
+
+    public void setCopy(Object copy) {
+        this.copy = copy;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "a=" + a +
+                ", b='" + b + '\'' +
+                ", copy=" + copy +
+                '}';
+    }
+}
