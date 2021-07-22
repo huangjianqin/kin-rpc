defb8627a9f21adb01383df44bdd29d83a00aa39
huangjianqin <18814127639@163.com>
2021-07-23 00:38:05 +0800

    Protobuf测试代码格式化

--- kin-rpc-serialization/kin-rpc-serialization-example/src/main/java/org/kin/kinrpc/serialization/ProtoBufSerializationTest.java
+++ kin-rpc-serialization/kin-rpc-serialization-example/src/main/java/org/kin/kinrpc/serialization/ProtoBufSerializationTest.java
@@ -12,14 +12,16 @@ import java.io.IOException;
  */
 public class ProtoBufSerializationTest {
     public static void main(String[] args) throws IOException {
-        Protobufs.register(ProtoBufMessageOuterClass.ProtoBufMessage.getDefaultInstance());
+        Protobufs.register(org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage.getDefaultInstance());
 
         ProtobufSerialization serialization = new ProtobufSerialization();
-        ProtoBufMessageOuterClass.ProtoBufMessage copy = ProtoBufMessageOuterClass.ProtoBufMessage.newBuilder().setA(2).setB("empty").build();
-        ProtoBufMessageOuterClass.ProtoBufMessage origin = ProtoBufMessageOuterClass.ProtoBufMessage.newBuilder().setA(1).setB("aa").setData(Any.pack(copy)).build();
+        org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage copy = org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage.newBuilder().setA(2).setB("empty").build();
+        org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage origin = org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage.newBuilder().setA(1).setB("aa").setData(Any.pack(copy)).build();
         byte[] bytes = serialization.serialize(origin);
         System.out.println(origin);
-        ProtoBufMessageOuterClass.ProtoBufMessage deserialize = serialization.deserialize(bytes, ProtoBufMessageOuterClass.ProtoBufMessage.class);
+        System.out.println(bytes.length);
+
+        org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage deserialize = serialization.deserialize(bytes, org.kin.kinrpc.serialization.ProtoBufMessageOuterClass.ProtoBufMessage.class);
         System.out.println(deserialize);
     }
 }
