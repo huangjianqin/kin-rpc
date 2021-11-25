c2137d303d2b01be896d5bb8782716c693a86912
huangjianqin <18814127639@163.com>
2021-11-25 19:23:22 +0800

    引入spring-boot-configuration-processor自动生成spring配置元数据

--- kin-rpc-spring/src/main/java/org/kin/kinrpc/spring/RedisRegistry.java
+++ kin-rpc-spring/src/main/java/org/kin/kinrpc/spring/RedisRegistry.java
@@ -7,6 +7,8 @@ import java.lang.annotation.*;
 /**
  * kinrpc 注册中心注解, 不能单独使用, 需配合{@link KinRpcService}或者{@link KinRpcReference}
  * 配置详情, 请看{@link org.kin.kinrpc.config.RedisRegistryConfig}
+ * <p>
+ * todo 考虑使用redis publish-subscribe
  *
  * @author huangjianqin
  * @date 2020/12/13
