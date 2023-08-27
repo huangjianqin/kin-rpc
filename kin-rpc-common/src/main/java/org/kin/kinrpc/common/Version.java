5501f6f87eaa37f3634d06b88045dea89bd6e7d2
huangjianqin <459564034@qq.com>
2023-08-27 13:48:18 +0800

    2.0版大改动4.73 代码优化

--- kin-rpc-common/src/main/java/org/kin/kinrpc/common/Version.java
+++ kin-rpc-common/src/main/java/org/kin/kinrpc/common/Version.java
@@ -2,14 +2,9 @@ package org.kin.kinrpc.common;
 
 public final class Version {
     private static String VERSION = "0.2.0.0";
-    private static String BUILD_TIME = "2023-08-27 13:43:14";
 
 
     public static String getVersion() {
         return VERSION;
-    }
-
-    public static String getBuildTime() {
-        return BUILD_TIME;
     }
 }
